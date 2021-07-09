c8004ca16fba08e14585a6f95c72388bcf6fd360
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-07-09 14:53:31 -0300

    Remoção dos comentários de linha do arquivo de teste.

--- src/Lexer.java
+++ src/Lexer.java
@@ -8,9 +8,9 @@ public class Lexer/*@bgen(jjtree)*/implements LexerTreeConstants, LexerConstants
   protected static JJTLexerState jjtree = new JJTLexerState();public static void execute(Lexer analisador) throws ParseException {
                 SimpleNode node = analisador.main();
                 // node.dump("->");
-                System.out.println("\u005cn ------------------------------------------");
+                System.out.println("\u005cn  ------------------------------------------");
             System.out.println("   Mas ah indio veio, que escrita bagual!");
-            System.out.println(" ------------------------------------------\u005cn");
+            System.out.println("  ------------------------------------------\u005cn");
         }
 
         public static void extensionCheck(String file) throws Throwable {
