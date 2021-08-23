4ca3138dcf7205bd39a3c2f55bf9304cce00ea8a
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-08-23 01:57:58 -0300

    Melhorias na organização do código da IDE.

--- galpao/interface.py
+++ galpao/interface.py
@@ -21,10 +21,6 @@ left_frame = tk.Frame(main_frame, background=BAR_BACKGROUND)
 rigth_frame = tk.Frame(main_frame, background=BAR_BACKGROUND)
 bottom_frame = tk.Frame(main_frame, background=BAR_BACKGROUND)
 linenumber_text = tk.Text(left_frame, bg='#191a21', foreground='grey', width=5, highlightthickness=0, borderwidth=0, pady=INTERNAL_PADDING)
-linenumber_text.insert(1.0, '1')
-linenumber_text.tag_configure("center", justify='center')
-linenumber_text.tag_add("center", "1.0", "end")
-linenumber_text.configure(state='disabled')
 scrollbar = tk.Scrollbar(
         left_frame, bg='#505050', troughcolor=BACKGROUND, highlightcolor="gray",
         activebackground="gray", highlightbackground=BAR_BACKGROUND,
@@ -34,15 +30,7 @@ code_text = tk.Text(
         selectforeground=FOREGROUND, tabs=16,  highlightthickness=0, borderwidth=0, pady=INTERNAL_PADDING, padx=INTERNAL_PADDING,
         insertbackground=FOREGROUND)
 console_text = tk.Text(bottom_frame, background=BACKGROUND, foreground=FOREGROUND,  pady=INTERNAL_PADDING, padx=INTERNAL_PADDING, height=5, highlightthickness=0, borderwidth=0)
-console_text.insert(1.0, 'Desentrevador\nAguardando processo de compilação...')
-console_text.configure(state='disabled')
 tree_label = tk.Text(rigth_frame, background=BACKGROUND, foreground=FOREGROUND,  pady=INTERNAL_PADDING, padx=INTERNAL_PADDING, width=6, highlightthickness=0, borderwidth=0)
-tree_label.insert(1.0, 'Agurdando árvore sintática...')
-tree_label.configure(state='disabled')
-
-# ********** HIGHLIGHT CONFIGURATION ********** #
-code_text.tag_configure("types", foreground="#bf00ff")
-code_text.tag_configure("reserved", foreground="#ffa500")
 
 # ********** WINDOW CONFIGURATION ********** #
 window.geometry(f'{window.winfo_screenwidth()}x{window.winfo_screenheight()}')
@@ -72,26 +60,36 @@ window.config(menu=menu_bar)
 left_frame.grid(sticky="nsew", row=0, column=0, padx=(EXTERNAL_PADDING, EXTERNAL_PADDING // 2), pady=(EXTERNAL_PADDING, EXTERNAL_PADDING // 2))
 left_frame.grid_columnconfigure(0, weight=1)
 left_frame.grid_rowconfigure(1, weight=1)
-code_text.configure(yscrollcommand=lambda x, y: utils.text_scroll_control(x, y, scrollbar, linenumber_text))
+linenumber_text.insert(1.0, '1')
+linenumber_text.tag_configure("center", justify='center')
+linenumber_text.tag_add("center", "1.0", "end")
+linenumber_text.configure(state='disabled')
 linenumber_text.pack(side="left", fill="y")
-scrollbar.pack(side='right', fill='y')
-code_text.pack(side='top', fill='both', expand=True)
-code_text.focus_set()
 scrollbar.config(command=lambda x, y: utils.scroll_control(x, y, code_text, linenumber_text))
+scrollbar.pack(side='right', fill='y')
+code_text.configure(yscrollcommand=lambda x, y: utils.text_scroll_control(x, y, scrollbar, linenumber_text))
+code_text.tag_configure("types", foreground="#bf00ff")
+code_text.tag_configure("reserved", foreground="#ffa500")
 code_text.bind('<BackSpace>', lambda key: utils.linenumber_control(key, code_text, linenumber_text))
 code_text.bind('<Return>', lambda key: utils.linenumber_control(key, code_text, linenumber_text))
 code_text.bind('<Any-KeyPress>', lambda _: utils.apply_highlight(code_text))
+code_text.pack(side='top', fill='both', expand=True)
+code_text.focus_set()
 
 # ********** BOTTOM OUTPUT AREA CONFIGURATION ********** #
 rigth_frame.grid(row=0, column=1, sticky='nsew', padx=(EXTERNAL_PADDING // 2, EXTERNAL_PADDING), pady=(EXTERNAL_PADDING, EXTERNAL_PADDING // 2))
 rigth_frame.grid_columnconfigure(0, weight=1)
 rigth_frame.grid_rowconfigure(1, weight=1)
+console_text.insert(1.0, 'Desentrevador\nAguardando processo de compilação...')
+console_text.configure(state='disabled')
 console_text.pack(fill='both', expand=True)
 
 # ********** RIGTH TREE AREA CONFIGURATION ********** #
 bottom_frame.grid(row=1, column=0, sticky='nsew', padx=(EXTERNAL_PADDING, EXTERNAL_PADDING), pady=(EXTERNAL_PADDING // 2, EXTERNAL_PADDING), columnspan=2)
 bottom_frame.grid_columnconfigure(0, weight=1)
 bottom_frame.grid_rowconfigure(1, weight=1)
+tree_label.insert(1.0, 'Agurdando árvore sintática...')
+tree_label.configure(state='disabled')
 tree_label.pack(fill='both', expand=True)
 
 # RUN FIXED WINDOW
